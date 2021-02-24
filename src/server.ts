1499082ede7362f68114614cb8fb1fa2eb68d101
fernandamello <fernanda.mello02@edu.pucrs.br>
2021-02-23 21:22:47 -0300

    Segundo dia do NLW finalizado

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
 
-app.get('/teste', (req, res) => {
-    return res.json({ message: 'Hello world!' });
-})
-
-app.post('/teste', (req, res) => {
-    return res.json({ message: 'Os dados foram salvos com sucesso' })
-})
+app.use(express.json())
+app.use(router);
 
 app.listen(3333, () => {
     console.log('Server is running on port ' + 3333);
